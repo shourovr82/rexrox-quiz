6bba25c968bf536a4aa385d8730a6d347078ebc8
shourovr82 <shafinur512@gmail.com>
2022-10-10 21:30:05 +0600

    header and error element created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import ErrorPage from './Layout/ErrorPage';
+import Main from './Layout/Main';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/', element: <Main></Main>,
+      errorElement: <ErrorPage></ErrorPage>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        }
+      ]
+    }
+  ])
+
+
   return (
     <div className="App">
-      <h2>Shafin</h2>
+      <RouterProvider router={router}>
+
+      </RouterProvider>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div>
-
+      <div className="navbar bg-emerald-800 text-white">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl" href='/'>daisyUI</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal p-0">
+            <li>
+              <Link to='/home'> Item 1</Link></li>
+            <li tabIndex={0}>
+              <a href='/'>
+                Parent
+                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+              </a>
+              <ul className="p-2 bg-base-100">
+                <li><a href='/'>Submenu 1</a></li>
+                <li><a href='/'>Submenu 2</a></li>
+              </ul>
+            </li>
+            <li><a href='/'>Item 3</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
